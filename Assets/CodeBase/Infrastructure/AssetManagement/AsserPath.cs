8485b70b0d08dcfa397e0db9a39aa8a850609c77
MalovePi <p.malove@yandex.ru>
2022-09-15 00:17:48 +0300

    Dependency injection

---
+++ Assets/CodeBase/Infrastructure/AssetManagement/AsserPath.cs
@@ -0,0 +1,8 @@
+namespace CodeBase.Infrastructure.AssetManagement
+{
+    public static class AsserPath
+    {
+        public const string DogHeroPath = "DogKnight/DogHero";
+        public const string HudPath = "Hud/Hud";
+    }
+}
---
+++ Assets/CodeBase/Infrastructure/States/IState.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeBase.Infrastructure.States
+{
+    public interface IState : IExitState
+    {
+        void Enter();
+        
+    }
+
+    public interface IPayloadsState<TPayload> : IExitState
+    {
+        void Enter(TPayload payload);
+    }
+    
+    public interface IExitState
+    {
+        void Exit();
+    }
+}
